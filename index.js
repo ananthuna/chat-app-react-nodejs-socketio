3d107487115ae07367b988abe7f58dc6ca3a569e
ananthu <ananthu4uuu@gmail.com>
2022-10-26 09:23:01 +0530

    node_modules removed

--- index.js
+++ index.js
@@ -18,13 +18,13 @@ const port = process.env.PORT || 8080
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-}));
+// app.use(cors({
+//     origin: 'http://localhost:3000',
+//     credentials: true,
+// }));
 app.use(sessionMiddlewear)
 app.use(cookieParser());
-// app.use(express.static('build'));
+app.use(express.static('build'));
 app.use("/public", express.static(path.join(__dirname, 'public')));
 
 
